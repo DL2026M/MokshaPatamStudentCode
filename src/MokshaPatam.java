fcbfc2cd7300c787950f736c8acf353ece53409e
davidlutch <david.lutch@menloschool.org>
2025-09-08 20:45:52 -0700

    Got it working! Just need to update work log and add the rest of my comments!

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -22,8 +22,17 @@ public class MokshaPatam {
         int currentNode = 0;
         int Node = 0;
         Queue<Integer> order = new LinkedList<Integer>();
-        //int snake[] =
 
+        int[] snake = new int[boardsize + 1];
+        int[] ladder = new int[boardsize + 1];
+        int[] rollTracker = new int [boardsize + 1];
+        // WRITE A COMMENT HERE TO EXPLAIN THIS
+        for (int i = 0; i < ladders.length; i++) {
+            ladder[ladders[i][0]] = ladders[i][1];
+        }
+        for (int i = 0; i < snakes.length; i++) {
+            snake[snakes[i][0]] = snakes[i][1];
+        }
 
         // BFS is going to remove the first node and then see what are the possible places we can move
         order.add(1);
@@ -32,18 +41,28 @@ public class MokshaPatam {
             // Checking to see if we are at the end of the board
             if (currentNode == boardsize) {
                 // Return the number of rolls
-                return fewestRolls;
+                return rollTracker[currentNode];
             }
             // Going through all the different dice roll possibilities
             for (int i = 1; i <= 6; i++) {
                 // moves to the next node over
                 Node = currentNode + i;
-                // CHECK IF THE NODE IS A SNAKE OR A LADDER
-                // FIGURE OUT A WAY TO KNOW HOW MANY ROLLS TO GET TO THE NODE WE ARE CURRENTLY VISITING
-                // no clue how to save how many rolls we have used
-                // whatever node that is visited I need to add it to the queue
+                // Checks to see if the node isn't a snake
+                if (Node <= boardsize) {
+                    if (snake[Node] != 0) {
+                        Node = snake[Node];
+                    } else if (ladder[Node] != 0) {
+                        Node = ladder[Node];
+                    }
+                    // Checking to see if the node has been visited
+                    if (rollTracker[Node] == 0) {
+                        rollTracker[Node] += rollTracker[currentNode] + 1;
+                        order.add(Node);
+                    }
+                }
             }
         }
-        return 0;
+        return -1;
     }
+
 }
